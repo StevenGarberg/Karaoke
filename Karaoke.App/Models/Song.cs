6d97c60ed191d08896fcbff89261f8e28c02826e
Steven Garberg <steven.garberg.jr@gmail.com>
2022-05-27 19:11:16 -0400

    Imported project in current state

---
+++ Karaoke.App/Models/Song.cs
@@ -0,0 +1,9 @@
+﻿namespace Karaoke.App.Models;
+
+public class Song
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string Title { get; set; }
+    public string Artist { get; set; }
+    public string Url { get; set; }
+}
---
+++ Karaoke.App/Repositories/SongRepository.cs
@@ -0,0 +1,76 @@
+﻿using Karaoke.App.Models;
+
+namespace Karaoke.App.Repositories;
+
+public class SongRepository
+{
+    private readonly HttpClient _httpClient;
+    private List<Song> _songs = new();
+    
+    public SongRepository(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+        // TODO: Read in songs from file and cache
+        /*
+        _songs.Add(new Song
+        {
+            Title = "Uneasy Hearts Weigh The Most",
+            Artist = "Dance Gavin Dance",
+            Url = "https://youtube.com/dgd-uneasy"
+        });
+        _songs.Add(new Song
+        {
+            Title = "Tree Village",
+            Artist = "Dance Gavin Dance",
+            Url = "https://youtube.com/dgd-tree"
+        });
+        */
+    }
+    
+    public async Task <IEnumerable<Song>> GetAll()
+    {
+        if (!_songs.Any())
+        {
+            var csv = await _httpClient.GetStringAsync("songs.csv");
+            var lines = csv.Split("\r\n");
+            
+            var id = 0;
+            var currentArtist = string.Empty;
+            foreach (var line in lines)
+            {
+                var columns = line.Split(",");
+                if (!string.IsNullOrWhiteSpace(columns[0]))
+                {
+                    currentArtist = columns[0].Trim().Replace(":", "");
+                }
+                else if (!string.IsNullOrWhiteSpace(columns[1]) && !string.IsNullOrWhiteSpace(columns[2]))
+                {
+                    _songs.Add(new Song
+                    {
+                        Id = (++id).ToString(),
+                        Title = columns[1].Trim(),
+                        Artist = currentArtist,
+                        Url = columns[2].Trim()
+                    });
+                }
+                else continue;
+            }
+
+            //_songs = _songs.OrderBy(x => x.Artist).ThenBy(x => x.Title).ToList();
+        }
+            
+        return _songs;
+    }
+
+    public async Task<IEnumerable<Song>> GetAllByArtist(string artist)
+    {
+        return (await GetAll())
+            .Where(s => string.Equals(s.Artist, artist, StringComparison.OrdinalIgnoreCase));
+    }
+
+    public async Task<Song> GetByTitle(string title)
+    {
+        return (await GetAll())
+            .FirstOrDefault(s => string.Equals(s.Title, title, StringComparison.OrdinalIgnoreCase));
+    }
+}
